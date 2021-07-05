f5d45ab89e46efe5f318e6edaf06eea008e1e1fb
Yusuff17 <neyan1017@gmail.com>
2021-07-05 10:50:37 +0530

    ToDoList App

---
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.todolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    private ArrayList<String> items;
+    private ArrayAdapter<String>itemsAdapter;
+    private ListView listview;
+    private Button button;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listview = findViewById(R.id.listview);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                additem(view);
+            }
+        });
+
+        items = new ArrayList<>();
+        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listview.setAdapter(itemsAdapter);
+        setUpListViewListener();
+    }
+
+    private void setUpListViewListener() {
+        listview.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int i, long l) {
+                Context context = getApplicationContext();
+                Toast.makeText(context,"item removed", Toast.LENGTH_LONG).show();
+
+
+                items.remove(i);
+                itemsAdapter.notify();
+                return true;
+            }
+
+        });
+
+    }
+
+    private void additem(View view) {
+        EditText input = findViewById(R.id.editText2);
+        String itemText = input.getText().toString();
+
+        if(!(itemText.equals(" "))){
+            itemsAdapter.add(itemText);
+            input.setText(" ");
+        }
+        else{
+            Toast.makeText(getApplicationContext(), "Please enter text. .", Toast.LENGTH_LONG).show();
+        }
+    }
+}
